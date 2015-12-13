f02ac6f459e700fdc14e0ba8afc8a3c0ac970edf
vconst <kos4ru@gmail.com>
2015-12-13 20:40:03 +0400

    applying simple attributes

--- src/vdom.js
+++ src/vdom.js
@@ -1,14 +1,44 @@
-"use strict";
 (function(window, undefined) {
+"use strict";
 
 var v = window.v = function(selector, attrs, children) {
+	if(selector instanceof v) {
+		selector.init(selector.node, attrs, children);
+		return selector;
+	}
+
+	if(selector instanceof Element && selector.v) {
+		selector.v.init(selector, attrs, children);
+		return selector.v;
+	}
+
     return new v.fn.init(selector, attrs, children);
 };
 
 v.fn = v.prototype = {
     version: "0.1.0",
-    init: function(selector) {
-        this.selector = selector;
+    init: function(selector, attrs, children) {
+        this.attrs = attrs;
+        this.children = children;
+
+		if(selector instanceof Element) {
+			this.node = selector;
+			selector.v = this;
+			this.apply();
+		}
+		else {
+	        this.nodeType = selector;
+		}
+    },
+    apply: function() {
+    	var attrs = this.attrs,
+    		name;
+
+    	if(attrs) {
+    		for(name in attrs) {
+    			this.node.setAttribute(name, attrs[name]);
+    		}
+    	}
     }
 };
 
--- tests/tests.js
+++ tests/tests.js
@@ -7,7 +7,7 @@ QUnit.test("namespace", function(assert) {
 });
 
 QUnit.test("init", function(assert) {
-  var el = v("<div>");
+  var el = v("div");
 
   assert.ok(el, "vdom element is created" );
   assert.equal(typeof el, "object", "vdom element type" );
@@ -15,5 +15,51 @@ QUnit.test("init", function(assert) {
   assert.equal(el.version, "0.1.0", "current version");
 });
 
-})(v)
+QUnit.test("apply attributes on dom element", function(assert) {
+  var el = document.createElement("div");
 
+  var v1 = v(el, { id: "test-id", class: "test1 test2", style: "width: 200px; height: 100px" });
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.strictEqual(el.v, v1, "vdom element instance in dom" );
+  assert.ok(el.v instanceof v.fn.init, "vdom element is instance of v" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.getAttribute("class"), "test1 test2", "class attribute");
+  assert.equal(el.className, "test1 test2", "className property");
+  assert.equal(el.style.width, "200px", "style.width property");
+});
+
+QUnit.test("apply attributes on virtual dom element", function(assert) {
+  var el = document.createElement("div");
+  var v1 = v(el, { id: "test-id-old" });
+
+  var v2 =  v(v1, { id: "test-id-new", class: "test1 test2", style: "width: 200px; height: 100px" });
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.ok(el.v === v1, "vdom element instance in dom");
+  assert.ok(v1 === v2, "vdom element is not recreated");
+  assert.ok(el.v instanceof v.fn.init, "vdom element is instance of v" );
+  assert.equal(el.getAttribute("id"), "test-id-new", "id attribute");
+  assert.equal(el.getAttribute("class"), "test1 test2", "class attribute");
+  assert.equal(el.className, "test1 test2", "className property");
+  assert.equal(el.style.width, "200px", "style.width property");
+});
+
+QUnit.test("apply attributes on dom element several times", function(assert) {
+  var el = document.createElement("div");
+  var v1 = v(el, { id: "test-id-old" });
+
+  var v2 =  v(el, { id: "test-id-new", class: "test1 test2", style: "width: 200px; height: 100px" });
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.ok(el.v === v1, "vdom element instance in dom");
+  assert.ok(v1 === v2, "vdom element is not recreated");
+  assert.ok(el.v instanceof v.fn.init, "vdom element is instance of v" );
+  assert.equal(el.getAttribute("id"), "test-id-new", "id attribute");
+  assert.equal(el.getAttribute("class"), "test1 test2", "class attribute");
+  assert.equal(el.className, "test1 test2", "className property");
+  assert.equal(el.style.width, "200px", "style.width property");
+});
+
+
+})();
