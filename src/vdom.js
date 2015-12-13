4460d4b302da2ecf3fb2817cb7a413bc339348df
vconst <kos4ru@gmail.com>
2015-12-14 02:44:39 +0400

    support children

--- src/vdom.js
+++ src/vdom.js
@@ -2,6 +2,12 @@
 "use strict";
 
 var v = window.v = function(selector, attrs, children) {
+	if(selector instanceof window.Array) {
+		attrs = selector[1];
+		children = selector[2];
+		selector = selector[0];
+	}
+
 	if(selector instanceof v) {
 		selector.init(selector.node, attrs, children);
 		return selector;
@@ -10,6 +16,11 @@ var v = window.v = function(selector, attrs, children) {
 	if(selector instanceof Element && selector.v) {
 		selector.v.init(selector, attrs, children);
 		return selector.v;
+	}
+
+	if(attrs instanceof window.Array || typeof attrs === "string") {
+		children = attrs;
+		attrs = undefined;
 	}
 
 	return new v.fn.init(selector, attrs, children);
@@ -23,20 +34,47 @@ v.fn = v.prototype = {
 
 		if(selector instanceof Element) {
 			this.node = selector;
-			selector.v = this;
+			this.node.v = this;
 			this.apply();
 		}
 		else {
-			this.nodeType = selector;
+			this.tagName = selector;
 		}
+	},
+	build: function() {
+		this.node = document.createElement(this.tagName);
+		this.node.v = this;
+		this.apply();
 	},
 	apply: function() {
 		var attrs = this.attrs,
-			name;
+			children = this.children;
 
 		if(attrs) {
-			for(name in attrs) {
+			for(var name in attrs) {
 				this.node.setAttribute(name, attrs[name]);
+			}
+		}
+		this.node.textContent = "";
+		if(children) {
+			if(!(children instanceof window.Array)) {
+				children = [children];
+			}
+			for(var i = 0; i < children.length; i++) {
+				var vNode = children[i];
+				if(vNode) {
+					if(vNode instanceof window.Array && typeof vNode[0] === "string") {
+						vNode = v(vNode);
+					}
+					if(vNode instanceof v) {
+						vNode.build();
+						this.node.appendChild(vNode.node);
+					}
+					else if(typeof vNode === "string") {
+						var node = document.createTextNode(vNode);
+						this.node.appendChild(node);
+					}
+				}
 			}
 		}
 	}
--- tests/tests.js
+++ tests/tests.js
@@ -61,5 +61,95 @@ QUnit.test("apply attributes on dom element several times", function(assert) {
   assert.equal(el.style.width, "200px", "style.width property");
 });
 
+QUnit.test("apply text content", function(assert) {
+  var el = document.createElement("div");
+
+  v(el, { id: "test-id" }, "Test Text");
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.innerText, "Test Text", "id attribute");
+});
+
+QUnit.test("apply text content several times", function(assert) {
+  var el = document.createElement("div");
+
+  v(el, { id: "test-id" }, "Test Text 1");
+  v(el, { id: "test-id" }, "Test Text 2");
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.innerText, "Test Text 2", "inner text");
+});
+
+QUnit.test("apply children as virtual nodes", function(assert) {
+  var el = document.createElement("div");
+
+  v(el, { id: "test-id" }, [v("span", "Test Text 1"), v("span", "Test Text 2")]);
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.childNodes.length, 2, "children count");
+  assert.equal(el.childNodes[0].tagName, "SPAN", "child 1 node type");
+  assert.equal(el.childNodes[0].innerText, "Test Text 1", "children node 1 text");
+  assert.equal(el.childNodes[1].tagName, "SPAN", "child 2 node type");
+  assert.equal(el.childNodes[1].innerText, "Test Text 2", "children node 2 text");
+});
+
+QUnit.test("apply children as virtual node arrays", function(assert) {
+  var el = document.createElement("div");
+
+  v(el, { id: "test-id" }, [["span", "Test Text 1"], ["span", "Test Text 2"]]);
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.childNodes.length, 2, "children count");
+  assert.equal(el.childNodes[0].tagName, "SPAN", "child 1 node type");
+  assert.equal(el.childNodes[0].innerText, "Test Text 1", "children node 1 text");
+  assert.equal(el.childNodes[1].tagName, "SPAN", "child 2 node type");
+  assert.equal(el.childNodes[1].innerText, "Test Text 2", "children node 2 text");
+});
+
+QUnit.test("apply children as virtual nodes arrays and texts", function(assert) {
+  var el = document.createElement("div");
+
+  v([el, { id: "test-id" }, [["span", "Test Text 1"], "Test Text 2", ["span", "Test Text 3"]]]);
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.childNodes.length, 3, "children count");
+  assert.equal(el.innerHTML, "<span>Test Text 1</span>Test Text 2<span>Test Text 3</span>", "inner html");
+});
+
+QUnit.test("apply attrs and children by array", function(assert) {
+  var el = document.createElement("div");
+
+  v([el, { id: "test-id" }, [["span", "Test Text 1"], ["span", "Test Text 2"]]]);
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.childNodes.length, 2, "children count");
+  assert.equal(el.childNodes[0].tagName, "SPAN", "child 1 node type");
+  assert.equal(el.childNodes[0].innerText, "Test Text 1", "children node 1 text");
+  assert.equal(el.childNodes[1].tagName, "SPAN", "child 2 node type");
+  assert.equal(el.childNodes[1].innerText, "Test Text 2", "children node 2 text");
+});
+
+QUnit.test("apply children recursive", function(assert) {
+  var el = document.createElement("div");
+
+  v(el, { id: "test-id" }, [["div", [["span", "Test Text 1"], ["span", "Test Text 2"]]]]);
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.getAttribute("id"), "test-id", "id attribute");
+  assert.equal(el.childNodes.length, 1, "children count");
+  assert.equal(el.childNodes[0].tagName, "DIV", "child 1 node type");
+  assert.equal(el.childNodes[0].childNodes.length, 2, "children count");
+  assert.equal(el.childNodes[0].childNodes[0].tagName, "SPAN", "child 1 node type");
+  assert.equal(el.childNodes[0].childNodes[0].innerText, "Test Text 1", "children node 1 text");
+  assert.equal(el.childNodes[0].childNodes[1].tagName, "SPAN", "child 2 node type");
+  assert.equal(el.childNodes[0].childNodes[1].innerText, "Test Text 2", "children node 2 text");
+});
+
 
 })();
