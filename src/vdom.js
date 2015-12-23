33fa0ba7f0f5cc68f4e6d075c29d8935ac895ed5
vconst <kos4ru@gmail.com>
2015-12-24 03:19:59 +0400

    added hashCode calculation, added performance test

--- src/vdom.js
+++ src/vdom.js
@@ -8,7 +8,7 @@ var v = window.v = function(selector, attrs, children) {
 		selector = selector[0];
 	}
 
-	if(attrs instanceof window.Array || typeof attrs === "string") {
+	if(attrs instanceof window.Array || typeof attrs === "string" || attrs instanceof v) {
 		children = attrs;
 		attrs = undefined;
 	}
@@ -53,6 +53,45 @@ function isObject(obj) {
 	return Object.getPrototypeOf(obj) === Object.prototype;
 }
 
+function getStrHashCode(str){
+    // 1315423911=b'1001110011001111100011010100111'
+    var hash = 1315423911,
+		i,
+		ch;
+		
+    for (i = str.length - 1; i >= 0; i--) {
+        ch = str.charCodeAt(i);
+        hash ^= ((hash << 5) + ch + (hash >> 2));
+    }
+
+    return (hash & 0x7FFFFFFF);
+}
+
+function getNodeHashCode(vNode) {
+	var attrs = vNode.attrs,
+		children = vNode.children,
+		textContent = vNode.textContent || "",
+		hash = getStrHashCode(vNode.tagName || "");
+	
+	if(attrs) {
+		for(var name in attrs) {
+			hash ^= getStrHashCode(name + ":" + attrs[name]);
+		}
+	}
+
+	if(textContent) {
+		hash ^= getStrHashCode(textContent);
+		hash ^= getStrHashCode(":" + textContent);
+	}
+	
+	if(children) {
+		for(var index  = 0; index < children.length; index++) {
+			hash ^= getStrHashCode(index + ":" + children[index].hashCode);
+		}
+	}
+	return hash;
+}
+
 v.fn = v.prototype = {
 	version: "0.1.0",
 	init: function(selector, attrs, children) {
@@ -60,12 +99,15 @@ v.fn = v.prototype = {
 		this.children = children;
 
 		if(selector instanceof Element) {
+			//this.tagName = selector.tagName;
 			this.node = selector;
 			this.node.v = this;
+			this.normalize();
 			this.apply();
 		}
 		else if(typeof selector === "string"){
 			this.tagName = selector;
+			this.normalize();
 		}
 		else {
 			throw new Error("Wrong selector type");
@@ -73,6 +115,7 @@ v.fn = v.prototype = {
 	},
 	build: function() {
 		var tagName = this.tagName;
+			
 		if(tagName[0] === '<' && tagName[tagName.length - 1] === '>') {
 			this.node = document.createElement(tagName.slice(1, -1));
 		}
@@ -82,24 +125,20 @@ v.fn = v.prototype = {
 		this.node.v = this;
 		this.apply();
 	},
-	apply: function() {
+	normalize: function() {
 		var attrs = this.attrs,
 			children = this.children;
 
 		if(attrs) {
 			for(var name in attrs) {
-				var attrValue = attrs[name];
 				var attrHook = v.attrHooks[name];
 				
 				if(attrHook) {
-					attrValue = attrHook(attrValue);
+					attrs[name] = attrHook(attrs[name]);
 				}
-				this.node.setAttribute(name, attrValue);
 			}
 		}
-		if(this.node.nodeName !== "#text") {
-			this.node.textContent = "";
-		}
+		
 		if(children) {
 			if(!(children instanceof window.Array)) {
 				children = [children];
@@ -107,8 +146,40 @@ v.fn = v.prototype = {
 			if(children[0] && children[0][0] === "<") {
 				children = [children];
 			}
+			if(children.length === 1 && (typeof children[0] === "string")) {
+				this.textContent = children[0];
+				children = [];
+			}
+			this.children = children;
 			for(var i = 0; i < children.length; i++) {
-				var vNode = v(children[i]);
+				children[i] = v(children[i]);
+				children[i].normalize();
+			}
+		}
+		this.hashCode = getNodeHashCode(this);
+	},
+	apply: function() {
+		var attrs = this.attrs,
+			children = this.children,
+			textContent = this.textContent;
+
+		if(attrs) {
+			for(var name in attrs) {
+				this.node.setAttribute(name, attrs[name]);
+			}
+		}
+		
+		if(this.node.nodeName !== "#text") {
+			this.node.textContent = "";
+		}
+
+		if(textContent) {
+			this.node.textContent = textContent;
+		}
+		
+		if(children) {
+			for(var i = 0; i < children.length; i++) {
+				var vNode = children[i];
 
 				vNode.build();
 				this.node.appendChild(vNode.node);
--- tests/tests.js
+++ tests/tests.js
@@ -97,6 +97,17 @@ QUnit.test("apply text content several times", function(assert) {
   assert.equal(el.innerText, "Test Text 2", "inner text");
 });
 
+QUnit.test("apply one children as virtual node", function(assert) {
+  var el = document.createElement("div");
+
+  v(el, v("<span>", "Test Text 1"));
+
+  assert.ok(el.v, "vdom element is created" );
+  assert.equal(el.childNodes.length, 1, "children count");
+  assert.equal(el.childNodes[0].tagName, "SPAN", "child 1 node type");
+  assert.equal(el.childNodes[0].innerText, "Test Text 1", "children node 1 text");
+});
+
 QUnit.test("apply children as virtual nodes", function(assert) {
   var el = document.createElement("div");
 
@@ -166,5 +177,94 @@ QUnit.test("apply children recursive", function(assert) {
   assert.equal(el.childNodes[0].childNodes[1].innerText, "Test Text 2", "children node 2 text");
 });
 
+QUnit.test("hashCode for simple node", function(assert) {
+  assert.ok(v("<div>").hashCode > 0, "hashCode is positive number");
+  
+  assert.equal(v("<div>").hashCode, v("<div>").hashCode, "hashCodes for equal tagName");
+  assert.notEqual(v("<div>").hashCode, v("<span>").hashCode, "hashCodes for not equal tagName");
+  
+  assert.equal(v("<div>", { id: "test1", style: { display: "none", width: "100px" }, class: { test1: true, test2: true } }).hashCode, 
+               v("<div>", { id: "test1", style: { display: "none", width: "100px" }, class: { test1: true, test2: true } }).hashCode, "hashCodes for equal attributes id, class, style");
+
+  assert.notEqual(v("<div>", { id: "test1", style: { display: "none", width: "100px" }, class: { test1: true, test2: true } }).hashCode, 
+                  v("<div>", { id: "test2", style: { display: "none", width: "100px" }, class: { test1: true, test2: true } }).hashCode, "hashCodes for not equal id attribute");
+  
+  assert.notEqual(v("<div>", { id: "test1", style: { display: "none", width: "100px" }, class: { test1: true, test2: true } }).hashCode, 
+                  v("<div>", { id: "test1", style: { display: "none", width: "200px" }, class: { test1: true, test2: true } }).hashCode, "hashCodes for not equal style attribute");
+
+  assert.notEqual(v("<div>", { id: "test1", style: { display: "none", width: "100px" }, class: { test1: true, test2: true } }).hashCode, 
+                  v("<div>", { id: "test1", style: { display: "none", width: "100px" }, class: { test1: true, test2: false } }).hashCode, "hashCodes for not equal class attribute");
+});
+
+QUnit.test("hashCode for node with children", function(assert) {
+  assert.equal(v("<div>", "Test1").hashCode, v("<div>", "Test1").hashCode, "hashCodes for equal text content");
+  assert.notEqual(v("<div>", "Test1").hashCode, v("<span>", "Test2").hashCode, "hashCodes for not equal text content");
+
+  assert.equal(v("<div>", [["<span>", "Test1"], ["<span>", "Test2"]]).hashCode, v("<div>", [["<span>", "Test1"], ["<span>", "Test2"]]).hashCode, "hashCodes for equal children");
+  assert.notEqual(v("<div>", [["<span>", "Test1"], ["<span>", "Test2"]]).hashCode, v("<div>", [["<span>", "Test1"]]).hashCode, "hashCodes for not equal children count");
+  assert.notEqual(v("<div>", [["<span>", "Test1"], ["<span>", "Test2"]]).hashCode, v("<div>", [["<span>", "Test2"], ["<span>", "Test1"]]).hashCode, "hashCodes for not equal children order");
+  assert.notEqual(v("<div>", [["<span>", "Test1"], ["<span>", "Test2"]]).hashCode, v("<div>", [["<span>", "Test1"], ["<span>", "Test3"]]).hashCode, "hashCodes for not equal children text content");
+  assert.notEqual(v("<div>", ["<div>", ["<span>", "Test1"]]).hashCode, v("<div>", ["<div>", ["<span>", "Test2"]]).hashCode, "hashCodes for not equal children recursive");
+});
+
+QUnit.test("render table performance", function(assert) {
+	var rows = [];
+	var rowCount = 100;
+	var columnCount = 100;
+  
+	for(var rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+		rows.push(["<tr>", []]);
+		for(var columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+			rows[rowIndex][1].push(["<td>", /*{ style: { "text-align": "center" } },*/ "Test " + rowIndex + " " + columnIndex]);
+		}
+	}
+	
+	var container = document.createElement("div");
+	container.style.visibility = "hidden";
+	document.body.appendChild(container);
+
+	var startDate = new Date();
+	var date = startDate;
+	
+	var vTable = v("<table>", rows);
+	assert.ok(true, "initialization time - " + (new Date() - date));
+	
+	
+	date = new Date();
+	v(container, vTable);
+	assert.ok(true, "render time - " + (new Date() - date));
+	
+	date = new Date();
+	container.clientWidth;
+	assert.ok(true, "reflow time - " + (new Date() - date));
+	assert.ok(true, "full time - " + (new Date() - startDate));
+
+
+
+	container.textContent = "";
+	container.clientWidth;
+
+	startDate = new Date();
+	date = startDate;
+	var table = document.createElement("table");
+	container.appendChild(table);
+	for(var rowIndex = 0; rowIndex < rowCount; rowIndex++) {
+		var row = document.createElement("tr");
+		table.appendChild(row);
+		
+		for(var columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+			var cell = document.createElement("td");
+			cell.textContent = "Test " + rowIndex + " " + columnIndex;
+			//cell.style.textAlign = "center";
+			row.appendChild(cell);
+		}
+	}
+	assert.ok(true, "native js render time - " + (new Date() - date));
+
+	date = new Date();
+	container.clientWidth;
+	assert.ok(true, "native js reflow time - " + (new Date() - date));
+	assert.ok(true, "native js full time - " + (new Date() - startDate));
+});
 
 })();
