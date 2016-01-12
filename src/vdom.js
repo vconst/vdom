3c7d64988cf8d7e37b7875438517ec906f64ab30
vconst <kos4ru@gmail.com>
2016-01-12 08:01:25 +0400

    added ko binding support

--- src/vdom.js
+++ src/vdom.js
@@ -106,13 +106,15 @@ function eventHook(node, eventType, value, prevValue) {
 }
 
 function isObject(obj) {
-	return Object.getPrototypeOf(obj) === Object.prototype;
+	return obj && Object.getPrototypeOf(obj) === Object.prototype;
 }
 
 function fetch(vNode) {
 	var i,
 		attributes,
 		childNodes;
+		
+	vNode.tagName = vNode.node.tagName;
 
 	if(!vNode.attrs) {
 		attributes = vNode.node.attributes || [];
@@ -139,20 +141,86 @@ function fetch(vNode) {
 	}
 }
 
+function applyAttrs(vDom) {
+  	var attrs = vDom.attrs,
+		prevAttrs = vDom.prevAttrs;
+
+	if(attrs) {
+		for(var name in attrs) {
+			var value = attrs[name];
+			var prevValue = prevAttrs && prevAttrs[name];
+			if(name.length > 2 && name.indexOf("on") === 0) {
+				eventHook(vDom.node, name.substr(2), value, prevValue);
+			}
+			else if(v.attrHooks[name]){
+				attrs[name] = v.attrHooks[name](vDom, value, prevValue);
+				if(attrs[name] === undefined) {
+					attrs[name] = value;
+				}
+			}
+			else {
+				if(vDom.node.setAttribute && value !== prevValue) {
+					vDom.node.setAttribute(name, value);
+				}
+			}
+		}
+	}
+
+}
+
+function applyChildren(vDom) {
+	var prevChildren = vDom.prevChildren || [],
+		children = vDom.children || [],
+		childrenCount = Math.max(prevChildren.length, children.length),
+		textContent = vDom.textContent;
+
+	if(!childrenCount && vDom.prevTextContent !== textContent) {
+		vDom.node.textContent = textContent;
+	}
+
+	vDom.prevChildren = undefined;
+	
+	for(var i = 0; i < childrenCount; i++) {
+		var vNode = children[i];
+		var prevVNode = prevChildren[i];
+		
+		if(!vNode) {
+			prevVNode.node.parentNode.removeChild(prevVNode.node);
+		}
+		else if(!prevVNode) {
+			vNode.build();
+			vDom.node.appendChild(vNode.node);
+		}
+		else if(prevVNode.tagName !== vNode.tagName) {
+			vNode.build();
+			prevVNode.node.parentNode.replaceChild(vNode.node, prevVNode.node);
+		}
+		else {
+			children[i] = prevVNode;
+			
+			prevVNode.init(vNode);
+			prevVNode.apply();
+		}
+	}
+}
+
 v.fn = v.prototype = {
 	version: "0.1.0",
 	init: function(selector, attrs, children) {
 		this.prevAttrs = this.attrs;
 		this.prevChildren = this.children;
+		this.prevTextContent = this.textContent;
 		
 		this.attrs = attrs;
+		this.children = children && children.slice ? children.slice(0) : children;
 
-		if(children) {
-			this.children = children.slice ? children.slice(0) : children;
-		}
-
-		if(selector instanceof Element || selector instanceof Text) {
+		if(selector instanceof v) {
 			this.tagName = selector.tagName;
+			this.attrs = selector.attrs;
+			this.textContent = selector.textContent;
+			this.children = selector.children;
+		}
+		else if(selector instanceof Element || selector instanceof Text) {
 			this.node = selector;
 			this.node.v = this;
 			this.apply();
@@ -170,6 +238,21 @@ v.fn = v.prototype = {
 			throw new Error("Wrong selector type");
 		}
 	},
+	clone: function(recursive) {
+		//this.normalize();
+		
+		var result = new v.fn.init(this);
+		
+		if(recursive && result.children) {
+			result.children = result.children.slice(0);
+			
+			for(var i = 0; i < result.children.length; i++) {
+				result.children[i] = result.children[i].clone(recursive);
+			}
+		}
+		
+		return result;
+	},
 	build: function() {
 		var tagName = this.tagName;
 			
@@ -184,7 +267,7 @@ v.fn = v.prototype = {
 		this.node.v = this;
 		this.apply();
 	},
-	normalize: function() {
+	normalize: function(recursive) {
 		var children = this.children;
 		
 		if(children) {
@@ -201,76 +284,168 @@ v.fn = v.prototype = {
 			this.children = children;
 			for(var i = 0; i < children.length; i++) {
 				children[i] = v(children[i]);
+				if(recursive) {
+					children[i].normalize();
+				}
 			}
 		}
 	},
 	apply: function() {
 		this.normalize();
-
-		var attrs = this.attrs,
-			prevAttrs = this.prevAttrs;
-
-		if(attrs) {
-			for(var name in attrs) {
-				var value = attrs[name];
-				var prevValue = prevAttrs && prevAttrs[name];
-				if(name.length > 2 && name.indexOf("on") === 0) {
-					eventHook(this.node, name.substr(2), value, prevValue);
-				}
-				else if(v.attrHooks[name]){
-					attrs[name] = v.attrHooks[name](this, value, prevValue);
-					if(attrs[name] === undefined) {
-						attrs[name] = value;
-					}
-				}
-				else {
-					this.node.setAttribute(name, value);
-				}
-			}
-		}
 		
-		var prevChildren = this.prevChildren || [],
-			children = this.children || [],
-			childrenCount = Math.max(prevChildren.length, children.length),
-			textContent = this.textContent;
+		applyAttrs(this);
+		applyChildren(this);
+	}
+};
 
-		if(!childrenCount && this.prevTextContent !== textContent) {
-			this.node.textContent = textContent;
-		}
+v.fn.init.prototype = v.fn;
 
-		this.prevChildren = undefined;
-		
-		for(var i = 0; i < childrenCount; i++) {
-			var vNode = children[i];
-			var prevVNode = prevChildren[i];
+
+v.bindingHandlers = {
+	"if": function(vNode, value) {
+		return !!value;
+	},
+	"ifnot": function(vNode, value) {
+		return !value;
+	},
+	"foreach": function(vNode, items, allBindings) {
+		var children = [],
+			childNode;
 			
-			if(!vNode) {
-				prevVNode.node.parentNode.removeChild(prevVNode.node);
+		if(vNode.children && items) {
+			for(var itemIndex = 0; itemIndex < items.length; itemIndex++) {
+				for(var childIndex = 0; childIndex < vNode.children.length; childIndex++) {
+					childNode = vNode.children[childIndex].clone();
+					childNode.$bindingContext = createBindingContext(items[itemIndex], vNode.$bindingContext, itemIndex);
+					children.push(childNode);
+				}
 			}
-			else if(!prevVNode) {
-				vNode.build();
-				this.node.appendChild(vNode.node);
-			}
-			else if(prevVNode.tagName !== vNode.tagName) {
-				vNode.build();
-				prevVNode.node.parentNode.replaceChild(vNode.node, prevVNode.node);
-			}
-			else {
-				children[i] = prevVNode;
-				
-				prevVNode.prevAttrs = prevVNode.attrs;
-				prevVNode.prevChildren = prevVNode.children;
-				prevVNode.prevTextContent = prevVNode.textContent;
-				
-				prevVNode.attrs = vNode.attrs;
-				prevVNode.children = vNode.children;
-				prevVNode.textContent = vNode.textContent;
-				prevVNode.apply();
+		}
+		vNode.children = children;
+	},
+	"with": function(vNode, value, allBindings) {
+		var childNode;
+		
+		if(vNode.children) {
+			for(var index = 0; index < vNode.children.length; index++) {
+				childNode = vNode.children[index];
+				childNode.$bindingContext = createBindingContext(value, vNode.$bindingContext);
 			}
 		}
 	}
 };
 
-v.fn.init.prototype = v.fn;
+function createBindingContext(data, parentContext, index) {
+	return {
+			$parents: parentContext ? parentContext.$parents.concat(parentContext.$data) : [],
+			$root: parentContext ? parentContext.$root : data,
+			$parent: parentContext ? parentContext.$data : null,
+			$data: data,
+			$parentContext: parentContext,
+			$index: index
+		};
+}
+
+function extend(baseObj, obj) {
+	for(var name in obj) {
+		baseObj[name] = obj[name];
+	}
+	return baseObj;
+}
+
+function getBindingHandler(bindingName) {
+	var bindingHandler = v.bindingHandlers[bindingName];
+	if(!bindingHandler && v.attrHooks[bindingName]) {
+		bindingHandler = v.bindingHandlers[bindingName] = function(vNode, value) {
+			var attrs = vNode.attrs;
+			if(!vNode.templateAttrs) {
+				vNode.templateAttrs = attrs;
+				attrs = vNode.attrs = extend({}, attrs);
+			}
+			attrs[bindingName] = value;
+		}
+	}
+	return bindingHandler;
+}
+
+var bindingsCache = {};
+
+function getBindings(vNode, bindingContext) {
+	var dataBindString = vNode.attrs && vNode.attrs["data-bind"],
+	    bindingsGetter;
+		
+	if(dataBindString) {
+		bindingsGetter = bindingsCache[dataBindString];
+		if(!bindingsGetter) {
+			bindingsGetter = bindingsCache[dataBindString] = new Function("$context", "with($context){with($data||{}){return{" + dataBindString + "}}}");
+		}
+		return bindingsGetter(bindingContext);
+	}
+}
+
+function applyBindingsCore(template, data, bindingContext) {
+	var vNode = template.clone();
+	
+	vNode.$template = template;
+	vNode.$bindingContext = bindingContext = template.$bindingContext || bindingContext || createBindingContext(data);
+	
+	var allBindings = getBindings(vNode, bindingContext),
+		bindingName,
+		bindingHandler;
+	
+	for(bindingName in allBindings) {
+		bindingHandler = getBindingHandler(bindingName);
+		
+		if(bindingHandler(vNode, allBindings[bindingName], allBindings) === false) {
+			return;
+		}
+	}
+
+	var children = vNode.children,
+		childNode;
+		
+	if(children) {
+		vNode.children = [];
+		for(var i = 0; i < children.length; i++) {
+			childNode = applyBindingsCore(children[i], bindingContext.$data, bindingContext);
+			if(childNode) {
+				vNode.children.push(childNode);
+			}
+		}
+	}
+			
+	return vNode;
+}
+
+v.fn.applyBindings = function(viewModel) {
+	var template = this.$template,
+		vTree;
+
+	if(viewModel === undefined && this.$bindingContext) {
+		viewModel = this.$bindingContext.$data;
+	}
+
+	if(template) {
+		vTree = applyBindingsCore(template, viewModel);
+		if(vTree) {
+			v(this.node, vTree.attrs, vTree.children || vTree.textContent);
+		}
+	}
+};
+
+v.applyBindings = function(viewModel, element) {
+	var template;
+	
+	element = element || document.body;
+	
+	if(!element.v || !element.v.$template) {
+		var template = v(element).clone(true);
+		
+		element.v.$template = template;
+	}
+	element.v.applyBindings(viewModel);
+}
+
+//http://plnkr.co/edit/itZBEmj18NCid8lNjxeO?p=preview
 
 })(window);
--- tests/tests.js
+++ tests/tests.js
@@ -371,6 +371,84 @@ QUnit.test("apply event hook on virtual dom element", function(assert) {
   assert.equal(mousedownCount, 1, "mousedown count" );
 });
 
+QUnit.test("applyBindings foreach/if/text/attr", function(assert) {
+  var el = document.getElementById("qunit-fixture");
+  
+  el.innerHTML = '<ul class="list" data-bind="foreach: items"><li class="list-item" data-bind="if: visible, attr: { id: \'list-item-\' + caption }"><span class="list-item-caption" data-bind="text: caption"></span>: <span class="list-item-content" data-bind="text: text"></li></ul>'
+  
+  v.applyBindings({ items: [{ caption: "1", text: "text 1", visible: true }, { caption: "2", text: "text 2", visible: false }, { caption: "3", text: "text 3", visible: true }] }, el);
+  
+  assert.equal(el.innerHTML, 
+	"<ul class=\"list\" data-bind=\"foreach: items\">" + 
+		"<li class=\"list-item\" data-bind=\"if: visible, attr: { id: 'list-item-' + caption }\" id=\"list-item-1\">" + 
+			"<span class=\"list-item-caption\" data-bind=\"text: caption\">1</span>: <span class=\"list-item-content\" data-bind=\"text: text\">text 1</span>" + 
+		"</li>" + 
+		"<li class=\"list-item\" data-bind=\"if: visible, attr: { id: 'list-item-' + caption }\" id=\"list-item-3\">" + 
+			"<span class=\"list-item-caption\" data-bind=\"text: caption\">3</span>: <span class=\"list-item-content\" data-bind=\"text: text\">text 3</span>" + 
+		"</li>" + 
+	"</ul>");
+	
+	v.applyBindings({ items: [] }, el);
+	
+  assert.equal(el.innerHTML, 
+	"<ul class=\"list\" data-bind=\"foreach: items\">" + 
+	"</ul>");
+	
+});
+
+QUnit.test("applyBindings perfomance", function(assert) {
+	var viewModel = { items: [] };
+	
+    var el = document.createElement("div");
+	
+	document.body.appendChild(el);
+  
+    el.innerHTML = '<ul id="v-list" data-bind="foreach: items"><li>v: <span data-bind="text: val"></span></li></ul>';
+	
+	var date = new Date();
+	v.applyBindings(viewModel, el);
+	assert.ok(true, new Date() - date);
+
+	date = new Date();
+	fill(10000);
+	el.clientWidth;
+	assert.ok(true, new Date() - date);
+
+	date = new Date();
+	update(10000);
+	el.clientWidth;
+	assert.ok(true, new Date() - date);
+
+	function fill(n) {
+		var i,
+			items = [];
+		for (i = 0; i < n; i += 1) {
+			items.push({
+				val: i
+			});
+		}
+		viewModel.items = items;
+		
+		v.applyBindings(viewModel, el);
+	}
+
+	function update(n) {
+		var i, 
+			item;
+		
+		for(var i = 0 ; i < n; i++) {
+			item = viewModel.items[i];
+			item.val = item.val + ' ' + item.val;
+		};
+
+		v.applyBindings(viewModel, el);
+	}
+	
+	//el.remove();
+	
+
+});
+
 
 QUnit.module("Performance");
 
